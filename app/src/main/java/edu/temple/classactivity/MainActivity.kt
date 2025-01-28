b642f73dbc4a0923d4596c354a3ec0bfbb3cb228
stan39049 <stan39049@gmail.com>
2025-01-28 12:11:23 -0500

    Set an event on click where the text on the button changes

--- app/src/main/java/edu/temple/classactivity/MainActivity.kt
+++ app/src/main/java/edu/temple/classactivity/MainActivity.kt
@@ -1,14 +1,24 @@
 package edu.temple.classactivity
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var clickMeButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        clickMeButton = findViewById(R.id.button2)
+
+        clickMeButton.setOnClickListener{
+            clickMeButton.text = "Button Clicked"
+        }
     }
 }
